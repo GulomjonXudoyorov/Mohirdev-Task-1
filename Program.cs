134cba2f2370361af89c898c887092019222dade
GulomjonXudoyorov <alisherov1ch0774@gmail.com>
2024-12-18 20:43:17 +0500

    Solved

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Doiraning radiusini kiriting:");
+decimal pi = (decimal)Math.PI;
+decimal radius = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Doiraning yuzi: {pi*radius*radius}");
+Console.WriteLine($"Aylana uzunligi: {2*pi*radius}");
